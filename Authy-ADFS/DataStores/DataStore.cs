1d60d71981bd549c5b21d479312cb197bcdd11ee
Jonathan Hardison <jmh@jonathanhardison.com>
2016-10-09 16:16:53 -0500

    Stubbing and filling out more methods in datastore class.

--- Authy-ADFS/DataStores/DataStore.cs
+++ Authy-ADFS/DataStores/DataStore.cs
@@ -8,40 +8,152 @@ namespace Authy_ADFS.DataStores
     {
         //Publics
 
-
-        public AuthyRegisteredUser GetUserRegistration(string inputUserPhone)
+        /// <summary>
+        /// Get's user registration from datastore by phone number.
+        /// </summary>
+        /// <param name="inputUserPhone"></param>
+        /// <returns></returns>
+        public AuthyRegisteredUser GetUserRegistration(string inputUserPhone, string inputUserEmail)
         {
-              
+            try
+            {
+                var inputFix = new AuthyRegisteredUser();
+                inputFix.UserPhone = inputUserPhone != null ? inputUserPhone : string.Empty;
+                inputFix.UserEmail = inputUserEmail != null ? inputUserEmail : string.Empty;
+
+                var response = this._GetUserRegistration(inputFix);
+
+                return response;
+            }
+            catch
+            {
+                throw;
+            }
+            
+                 
         }
 
-        public AuthyRegisteredUser GetUserRegistration(string inputUserEmail)
-        {
-       
-        }
-
+        /// <summary>
+        /// Get's user registration from datastore by provided AuthyRegisteredUser type.
+        /// </summary>
+        /// <param name="inputUserRegistration"></param>
+        /// <returns></returns>
         public AuthyRegisteredUser GetUserRegistration(AuthyRegisteredUser inputUserRegistration)
         {
-           
+            try
+            {
+                var response = this._GetUserRegistration(inputUserRegistration);
+
+                return response;
+            }
+            catch
+            {
+                throw;
+            }
+
         }
 
+        /// <summary>
+        /// Registers user in datastore by provided inputs for email, phone, and userID.
+        /// </summary>
+        /// <param name="inputUserEmail"></param>
+        /// <param name="inputUserPhone"></param>
+        /// <param name="inputUserID"></param>
+        /// <returns></returns>
         public bool RegisterUser(string inputUserEmail, string inputUserPhone, string inputUserID)
         {
+            try
+            {
+                //add logic to validate inputs are not null. This method expects them to be properly defined.
+                var inputFix = new AuthyRegisteredUser();
+                inputFix.UserEmail = inputUserEmail;
+                inputFix.UserPhone = inputUserPhone;
+                inputFix.UserID = inputUserID;
 
+                var response = this._RegisterUser(inputFix);
+
+                return response;
+
+            }
+            catch
+            {
+
+                throw;
+            }
         }
 
+        /// <summary>
+        /// Registers user in datastore by provided input of AuthyRegisteredUser type.
+        /// </summary>
+        /// <param name="inputUserRegistration"></param>
+        /// <returns></returns>
         public bool RegisterUser(AuthyRegisteredUser inputUserRegistration)
         {
+            try
+            {
+                //add logic to validate inputs are not null. This method expects them to be properly defined.
+                var response = this._RegisterUser(inputUserRegistration);
+
+                return response;
+
+            }
+            catch
+            {
+
+                throw;
+            }
 
         }
 
+        /// <summary>
+        /// Unregisters user in datastore by provided inputs of email, phone, and userid.
+        /// </summary>
+        /// <param name="inputUserEmail"></param>
+        /// <param name="inputUserPhone"></param>
+        /// <param name="inputUserID"></param>
+        /// <returns></returns>
         public bool UnRegisterUser(string inputUserEmail, string inputUserPhone, string inputUserID)
         {
+            try
+            {
+                //add logic to validate inputs are not null. This method expects them to be properly defined.
+                var inputFix = new AuthyRegisteredUser();
+                inputFix.UserEmail = inputUserEmail;
+                inputFix.UserPhone = inputUserPhone;
+                inputFix.UserID = inputUserID;
 
+                var response = this._UnRegisterUser(inputFix);
+
+                return response;
+
+            }
+            catch
+            {
+
+                throw;
+            }
         }
 
+        /// <summary>
+        /// Unregisters user in datastore by provided input of AuthyRegisteredUser type.
+        /// </summary>
+        /// <param name="inputUserRegistration"></param>
+        /// <returns></returns>
         public bool UnRegisterUser(AuthyRegisteredUser inputUserRegistration)
         {
+            try
+            {
+                //add logic to validate inputs are not null. This method expects them to be properly defined.
+                var response = this._UnRegisterUser(inputUserRegistration);
 
+                return response;
+
+            }
+            catch
+            {
+
+                throw;
+            }
         }
 
         //Privates
@@ -87,12 +199,18 @@ namespace Authy_ADFS.DataStores
 
         }
 
+        /// <summary>
+        /// Private method to perform actual retrieval from datastore.
+        /// </summary>
+        /// <param name="inputUserRegistration"></param>
+        /// <returns></returns>
         private AuthyRegisteredUser _GetUserRegistration(AuthyRegisteredUser inputUserRegistration)
         {
             try
             {
-                
-                
+                //maybe add logic to determin if null, and if null throw error.
+                var response = dataStoreInstance.GetRegisteredUser(inputUserRegistration);
+                return response;
 
             }
             catch
@@ -102,13 +220,41 @@ namespace Authy_ADFS.DataStores
 
         }
 
+        /// <summary>
+        /// Private method to perform actual registration to datastore.
+        /// </summary>
+        /// <param name="inputUserRegistration"></param>
+        /// <returns></returns>
         private bool _RegisterUser(AuthyRegisteredUser inputUserRegistration)
         {
+            try
+            {
+                var response = dataStoreInstance.CheckandRegisterUser(inputUserRegistration);
+                return response;
+            }
+            catch
+            {
+                throw;
+            }
 
         }
 
+        /// <summary>
+        /// Private method to perform actual deletion from datastore.
+        /// </summary>
+        /// <param name="inputUserRegistration"></param>
+        /// <returns></returns>
         private bool _UnRegisterUser(AuthyRegisteredUser inputUserRegistration)
         {
+            try
+            {
+                var response = dataStoreInstance.DeleteRegisteredUser(inputUserRegistration);
+                return response;
+            }
+            catch
+            {
+                throw;
+            }
 
         }
 
